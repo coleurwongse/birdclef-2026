98e2e0f030d463dd64a62a1987d42df23ccac919
Cole Urwongse <cole.urwongse@gmail.com>
2026-04-13 18:38:13 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 import os
 import numpy as np
 import tensorflow as tf
-import tensorflow.keras.layers as layers
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from tensorflow.keras.layers import AveragePooling2D, Input, Flatten, Add
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.models import Model
 import pandas as pd
 import sklearn
 
@@ -14,26 +19,25 @@ classes = class_csv['primary_label'].tolist()
 
 train_csv = pd.read_csv(os.path.join(BASE, 'train.csv'))
 for index, row in train_csv.iterrows():
+    if index == 3000:
+        break
     filename = row['filename'].replace('.ogg', '.npy')
     audio_path = os.path.join('mel-spectrograms', filename)
     label = row['primary_label']
     spectrogram = np.load(os.path.join('mel-spectrograms', filename))
-    frame_length = 128
+    frame_length = 216
     num_frames = spectrogram.shape[1]
     parts = []
     for i in range(0, num_frames, frame_length):
-        part = spectrogram[:, i:i + frame_length]
-        if part.shape[1] < frame_length:
-            pad_width = frame_length - part.shape[1]
-            part = np.pad(part, ((0, 0), (0, pad_width)), mode='constant')
-        audio_data.append(part)
-    labels.append(label)
+        part = spectrogram[i:i + frame_length]
+        if len(part) == frame_length:
+            audio_data.append(part)
+            labels.append(label)
     if index % 100 == 0:
         print(index)
 
 label_encoder = sklearn.preprocessing.LabelEncoder()
 encoded_labels = label_encoder.fit_transform(labels)
-
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(audio_data, encoded_labels, test_size=0.2, random_state=42)
 
 # Ensure all spectrograms have the same shape
@@ -45,33 +49,112 @@ X_test = [np.pad(spec, ((0, max_length - spec.shape[0]), (0, 0)), mode='constant
 X_train = np.array(X_train)
 X_test = np.array(X_test)
 
-model = tf.keras.models.Sequential([
-  layers.Conv2D(8, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D((2, 2)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D((2, 2)),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D((2, 2)),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D((2, 2)),
-  layers.Conv2D(128, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D((2, 2)),
-  layers.Conv2D(256, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D((2, 2)),
-  layers.Flatten(),
-  layers.Dropout(0.2),
-  layers.Dense(512, activation='relu'),
-  layers.Dense(len(classes), activation='softmax', name="outputs")
-])
+batch_size = 32
+epochs = 200
+data_augmentation = True
+num_classes = 10
+subtract_pixel_mean = True
+depth = 29
 
+img_height = 224
+img_width = 224
+
+def lr_schedule(epoch):
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate:', lr)
+    return lr
+
+num_classes = len(classes)
+
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+
+def resnet_v2(input_shape, depth, num_classes=10):
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n + 2')
+
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+    inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs, num_filters=num_filters_in, conv_first=True)
+
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:
+                    strides = 2
+            y = resnet_layer(x, num_filters=num_filters_in, kernel_size=1,
+                             strides=strides, activation=activation,
+                             batch_normalization=batch_normalization, conv_first=False)
+            y = resnet_layer(y, num_filters=num_filters_in, conv_first=False)
+            y = resnet_layer(y, num_filters=num_filters_out, kernel_size=1, conv_first=False)
+            if res_block == 0:
+                x = resnet_layer(x, num_filters=num_filters_out, kernel_size=1,
+                                 strides=strides, activation=None, batch_normalization=False)
+            x = Add()([x, y])
+        num_filters_in = num_filters_out
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes, activation='softmax', kernel_initializer='he_normal')(y)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+checkpoint = ModelCheckpoint(filepath='resnet.keras',
+                             monitor='val_accuracy',
+                             verbose=1,
+                             save_best_only=True)
+lr_scheduler = LearningRateScheduler(lr_schedule)
+lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
+callbacks = [checkpoint, lr_reducer, lr_scheduler]
+
+model = resnet_v2(input_shape=(216, 128, 1), depth=depth, num_classes=num_classes)
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-epochs = 25
-model.fit(X_train, y_train, epochs=25, batch_size=32, validation_data=(X_test, y_test))
-
-test_accuracy=model.evaluate(X_test, y_test,verbose=0)
-print(test_accuracy[1])
-
-model.save('audio_classification_model.h5')
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, callbacks=callbacks)
